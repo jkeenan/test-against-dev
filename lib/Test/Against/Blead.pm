32cfb92847005b5c68aba5315a3393fd30d04071
James E Keenan <jkeenan@cpan.org>
2017-12-09 18:55:41 -0500

    Write and test constructor and basic accessors.

--- lib/Test/Against/Blead.pm
+++ lib/Test/Against/Blead.pm
@@ -1,7 +1,16 @@
 package Test::Against::Blead;
 use strict;
+use 5.10.1;
 our $VERSION = '0.01';
 use Carp;
+use File::Path ( qw| make_path | );
+use File::Spec;
+
+# What args must be passed to constructor?
+# application top-level directory
+# Constructor will get path to top-level directory for application,
+# compose names of other directories in the tree, verify they exist or create
+# them as needed.
 
 sub new {
     my ($class, $args) = @_;
@@ -9,12 +18,46 @@ sub new {
 
     croak "Argument to constructor must be hashref"
         unless ref($args) eq 'HASH';
+    croak "Hash ref must contain 'application_dir' element"
+        unless $args->{application_dir};
+    croak "Could not locate $args->{application_dir}"
+        unless (-d $args->{application_dir});
 
-    my $data = {};
+    my $data;
+    for my $k (keys %{$args}) {
+        $data->{$k} = $args->{$k};
+    }
 
+    for my $dir (qw| src testing results |) {
+        my $fdir = File::Spec->catdir($data->{application_dir}, $dir);
+        unless (-d $fdir) { make_path($fdir, { mode => 0755 }); }
+        croak "Could not locate $fdir" unless (-d $fdir);
+        $data->{"${dir}_dir"} = $fdir;
+    }
+
+    #my $self = { %Fields, %{$data} };
     return bless $data, $class;
 }
 
+sub get_application_dir {
+    my $self = shift;
+    return $self->{application_dir};
+}
+
+sub get_src_dir {
+    my $self = shift;
+    return $self->{src_dir};
+}
+
+sub get_testing_dir {
+    my $self = shift;
+    return $self->{testing_dir};
+}
+
+sub get_results_dir {
+    my $self = shift;
+    return $self->{results_dir};
+}
 
 1;
 
---
+++ t/001-new.t
@@ -0,0 +1,31 @@
+# -*- perl -*-
+# t/001-load.t - check module loading and create testing directory
+use strict;
+use warnings;
+
+use Test::More qw(no_plan); # tests => 2;
+use File::Temp ( qw| tempdir |);
+use Data::Dump ( qw| dd pp | );
+
+BEGIN { use_ok( 'Test::Against::Blead' ); }
+
+my $tdir = tempdir(CLEANUP => 1);
+my $self;
+
+$self = Test::Against::Blead->new( {
+    application_dir         => $tdir,
+} );
+isa_ok ($self, 'Test::Against::Blead');
+#pp($self);
+
+my $top_dir = $self->get_application_dir;
+is($top_dir, $tdir, "Located top-level directory $top_dir");
+
+for my $dir ( qw| src testing results | ) {
+    my $fdir = File::Spec->catdir($top_dir, $dir);
+    ok(-d $fdir, "Located $fdir");
+}
+ok(-d $self->get_src_dir, "Got src directory");
+ok(-d $self->get_testing_dir, "Got testing directory");
+ok(-d $self->get_results_dir, "Got results directory");
+
