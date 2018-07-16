905414848bff74db7ef2b7d5c7a73fb4798535d5
James E Keenan <jkeenan@cpan.org>
2018-07-16 12:00:16 -0400

    CPAN version 0.11

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
-    version => { old => '0.09', new => '0.10' },
-    date    => { old => 'July 12 2018', new => 'July 15 2018' },
+    version => { old => '0.10', new => '0.11' },
+    date    => { old => 'July 15 2018', new => 'July 16 2018' },
     copyright => { old => '2018', new => '2018' },
 );
 
--- lib/Test/Against/Dev/ProcessPSV.pm
+++ lib/Test/Against/Dev/ProcessPSV.pm
@@ -1,7 +1,7 @@
 package Test::Against::Dev::ProcessPSV;
 use strict;
 use 5.14.0;
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 use Carp;
 use Data::Dump ( qw| dd pp | );
 use Text::CSV_XS;
