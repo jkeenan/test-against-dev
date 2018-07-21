23f2bdad105028691cb614e24d0805afd75489ab
James E Keenan <jkeenan@cpan.org>
2018-07-21 08:08:54 -0400

    CPAN version 0.12

--- .version_info.pl
+++ .version_info.pl
@@ -1,6 +1,6 @@
 %trans = (
-    version => { old => '0.10', new => '0.11' },
-    date    => { old => 'July 15 2018', new => 'July 16 2018' },
+    version => { old => '0.11', new => '0.12' },
+    date    => { old => 'July 16 2018', new => 'July 21 2018' },
     copyright => { old => '2018', new => '2018' },
 );
 
