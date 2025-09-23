bbc0ae7a9be2606086e639818e4573b65857de8c
James E Keenan <jkeenan@cpan.org>
2025-09-23 09:35:19 -0400

    preparing a release

--- .version_info.pl
+++ .version_info.pl
@@ -1,7 +1,7 @@
 %trans = (
-    version => { old => '0.12', new => '0.13' },
-    date    => { old => 'July 21 2018', new => 'September 23 2018' },
-    copyright => { old => '2018', new => '2018' },
+    version => { old => '0.13', new => '0.14' },
+    date    => { old => 'September 23 2018', new => 'September 23 2025' },
+    copyright => { old => '2018', new => '2025' },
 );
 
 1;
